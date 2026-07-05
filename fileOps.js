17ed5ad0b729a210f99be190dd1cfa9fef7a50ca
happydavid226 <happydavid226@gmail.com>
2026-07-05 17:05:44 +0200

    separated concerns about file I/O

---
+++ fileOps.js
@@ -0,0 +1,20 @@
+import fs from 'fs/promises';
+
+export async function loadTasks(){
+    try {
+        const tasksString = await fs.readFile('./tasks.json', 'utf-8');
+        const tasks = JSON.parse(tasksString);
+        return tasks;
+    } catch(err){
+        console.log(`error reading data or parsing to json`, err);
+    }
+}
+
+export async function writeTasks(tasks){
+    try {
+        await fs.writeFile('./tasks.json', JSON.stringify(tasks, null,2))
+        console.log(`finished writing object`);
+    } catch(err){
+        console.log(`error writing tasks`, err);
+    }
+}
