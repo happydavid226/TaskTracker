acc6e105944e186ae02f25fff7c38df0bfb89b6a
happydavid226 <happydavid226@gmail.com>
2026-07-06 12:02:12 +0200

    added markDone

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import { markInProgress } from "./markInProgress.js";
+import { markDone } from "./markDone.js";
 
 async function main(){
     let args = process.argv;
@@ -7,21 +7,17 @@ async function main(){
         console.log(`There must be only 2 arguments. the last argument should be an integer`);
         return;
     }
-    if(args[2].toLowerCase() != 'mark-in-progress'){
-        console.log("The command should be mark-in-progress <id>");
+    if(args[2].toLowerCase() != 'mark-done'){
+        console.log("The command should be mark-in-done <id>");
         return;
     }
     args.splice(0,3);
     let id = Number(args.splice(0,1)[0]);
-    if(isNaN(id)){
-        console.log(`id should be a number`);
-        return;
-    }
-    const data = await markInProgress(id);
-    if(typeof data === 'undefined' || data.length === 0){
-        console.error(`no thing was updated`);
-    } else {
-        console.log(`STATUS CODE : 200,  deleted : \n`, data);
+    try {
+        let updatedTasks = await markDone(id);
+        console.log(`the updated tasks are : `, updatedTasks);
+    } catch(err){
+        console.log(`an error happened `, err);
     }
 }
 
--- markDone.js
+++ markDone.js
@@ -1,3 +1,22 @@
-export default async function markDone(){
-    
+import { loadTasks, writeTasks } from "./fileOps.js";
+import { taskExists } from "./utils.js";
+
+export async function markDone(id){
+    id = Number(id);
+    if(typeof id !== 'number'){
+        throw new Error('id must be a number');
+    }
+    let tasks = await loadTasks();
+    if(!taskExists(id, tasks)){
+        throw new Error('task does not exist');
+    }
+    let updatedTasks = [];
+    tasks.forEach((task, index) => {
+        if(tasks[index]._id === id){
+            tasks[index]._status = "DONE";
+            updatedTasks.push(task);
+        }
+    });
+    await writeTasks(tasks);
+    return updatedTasks;
 }
